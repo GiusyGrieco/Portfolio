fc96facd4ce1e8dadbedec923cd073514335e0dd
~ Gius <70314219+teragramgius@users.noreply.github.com>
2023-10-04 17:58:43 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://giusygriecopress.github.io/portfolio',
+  site: 'https://giusygriecopress.github.io',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
